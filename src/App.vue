87822c1b25faf94b66d5f334720ee38cf4411e09
Tony Graf <tony.graf@gmail.com>
2019-02-08 01:34:16 -0800

    Beginnng of project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1>vue books.</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  /*font-family: 'Playfair Display', serif;*/
+  font-family: 'Libre Baskerville', serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 75%;
+  margin: 20px auto;
+}
+
+body {
+  background-color: #fcf1ed;
+background-image: url("https://www.transparenttextures.com/patterns/45-degree-fabric-light.png");
+/* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
+}
+
+h1 {
+  font-size: 7rem;
+}
+</style>
