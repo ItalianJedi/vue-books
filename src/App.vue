0f014c770d913bb76e6a8a0a219a6847bb127d6c
Tony Graf <tony.graf@gmail.com>
2019-02-09 01:32:01 -0800

    updated styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>vue books.</h1>
+    <h1 id="title">vue books.</h1>
     <router-view/>
   </div>
 </template>
@@ -17,15 +17,24 @@ export default {
   font-family: 'Libre Baskerville', serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  /*color: #2c3e50;*/
+  color: white;
+  text-shadow: 2px 2px #050505;
   width: 75%;
   margin: 20px auto;
 }
 
+#title {
+  text-shadow: 3px 3px #050505;
+}
+
 body {
   background-color: #fcf1ed;
-background-image: url("https://www.transparenttextures.com/patterns/45-degree-fabric-light.png");
-/* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
+  background-color: #c0bec2;
+  background-image: url("https://www.transparenttextures.com/patterns/black-linen-2.png");
+  /* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
+  /*background-image: url("https://www.transparenttextures.com/patterns/45-degree-fabric-light.png");*/
+  /* This is mostly intended for prototyping; please download the pattern and re-host for production environments. Thank you! */
 }
 
 h1 {
